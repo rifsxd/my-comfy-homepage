f3ea90832ead6923bb87519ee6dbc581498167a8
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-12-15 22:02:31 +0600

    now also showing date

--- js/greetings.js
+++ js/greetings.js
@@ -19,18 +19,24 @@ function getGreeting() {
     }
 }
 
-// Function to update the displayed time
+// Function to update the displayed time and date
 function updateTime() {
-    var currentTimeElement = document.getElementById("currentTime");
+    var currentDateTimeElement = document.getElementById("currentDateTime");
     var currentTime = new Date();
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
+    var day = currentTime.getDate();
+    var month = currentTime.getMonth() + 1; // Month is zero-based
+    var year = currentTime.getFullYear();
 
-    // Format hours and minutes with leading zero if less than 10
+    // Format hours, minutes, day, and month with leading zero if less than 10
     hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
+    day = day < 10 ? "0" + day : day;
+    month = month < 10 ? "0" + month : month;
 
-    currentTimeElement.textContent = hours + ":" + minutes;
+    var dateTimeString = hours + ":" + minutes + " - " + year + "-" + month + "-" + day;
+    currentDateTimeElement.textContent = dateTimeString;
 }
 
 // Display greeting, update search engine text, and show the current time on page load
