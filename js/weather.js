1bf4a9b1fcbc46aec0a453ee50b9aa061dab6333
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-12-20 03:05:54 +0600

    successfully implemented weather

--- js/weather.js
+++ js/weather.js
@@ -1,51 +1,61 @@
-// Get user's location and fetch weather information
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showWeather, handleLocationError);
-    } else {
-        console.error("Geolocation is not supported by this browser.");
+document.addEventListener('DOMContentLoaded', function () {
+    // Get user's location and fetch weather information
+    function getLocation() {
+        const weatherContainer = document.getElementById('weatherContainer');
+        const apiKey = '0b3fb444200819f7267081e9f04bf7c6';
+
+        if (navigator.geolocation) {
+            // Display loading message
+            weatherContainer.innerHTML = 'Huh? No Weather!?';
+
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const { latitude, longitude } = position.coords;
+                    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+                    fetch(apiUrl)
+                        .then(response => response.json())
+                        .then(data => {
+                            // Extract relevant weather information (e.g., temperature, description)
+                            const temperature = Math.round(data.main.temp - 273.15); // Convert Kelvin to Celsius
+                            const description = data.weather[0].description;
+
+                            // Display the weather information
+                            weatherContainer.innerHTML = `${temperature}°C, ${description}`;
+                        })
+                        .catch(error => {
+                            console.error('Error fetching weather data', error);
+                            weatherContainer.innerHTML = 'Error fetching weather data';
+                        });
+                },
+                handleLocationError
+            );
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+            weatherContainer.innerHTML = 'Geolocation is not supported';
+        }
     }
-}
 
-function showWeather(position) {
-    const { latitude, longitude } = position.coords;
-
-    // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-    const apiKey = '0b3fb444200819f7267081e9f04bf7c6';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Extract relevant weather information (e.g., temperature, description)
-            const temperature = Math.round(data.main.temp - 273.15); // Convert Kelvin to Celsius
-            const description = data.weather[0].description;
-
-            // Display the weather information
-            const weatherContainer = document.getElementById('weatherContainer');
-            weatherContainer.innerHTML = `Current Weather: ${temperature}°C, ${description}`;
-        })
-        .catch(error => console.error('Error fetching weather data', error));
-}
-
-function handleLocationError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            console.error("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            console.error("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            console.error("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            console.error("An unknown error occurred.");
-            break;
-        default:
-            console.error("An error occurred while getting user location.");
+    function handleLocationError(error) {
+        const weatherContainer = document.getElementById('weatherContainer');
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                weatherContainer.innerHTML = "User denied the request for Geolocation.";
+                break;
+            case error.POSITION_UNAVAILABLE:
+                weatherContainer.innerHTML = "Location information is unavailable.";
+                break;
+            case error.TIMEOUT:
+                weatherContainer.innerHTML = "The request to get user location timed out.";
+                break;
+            case error.UNKNOWN_ERROR:
+                weatherContainer.innerHTML = "An unknown error occurred.";
+                break;
+            default:
+                weatherContainer.innerHTML = "An error occurred while getting user location.";
+        }
     }
-}
 
-// Call the function to ask for location permission
-getLocation();
+    // Call the function to ask for location permission
+    getLocation();
+});
